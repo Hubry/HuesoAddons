db0c36c235ac6da29d00785b660f8e2c69a7759a
Hubry <Hubry@users.noreply.github.com>
2019-10-30 17:31:30 +0100

    [Build] and slight code reformat

--- src/main/java/hubry/huesoaddons/HuesoAddons.java
+++ src/main/java/hubry/huesoaddons/HuesoAddons.java
@@ -6,7 +6,6 @@ import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import xbony2.huesodewiki.api.IWikiRecipe;
@@ -14,7 +13,6 @@ import xbony2.huesodewiki.recipe.RecipeCreator;
 import xbony2.huesodewiki.recipe.recipes.CraftingRecipe;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
 
